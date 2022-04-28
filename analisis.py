474f577660b49effb022951f7a37516f3af70f93
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-04-28 09:33:42 +0200

    añado la varianza

--- analisis.py
+++ analisis.py
@@ -22,3 +22,21 @@ def media(df):
     print("Media de la variable Total: ", mediatotal,"\nMedia de la variable Attack: ", mediaataque,"\nMedia de la variable Defense: ", mediadefensa,"\nMedia de la variable HP: ", mediahp,"\nMedia de la variable Sp. Atk: ", mediaspatk,"\nMedia de la variable Sp. Def: ", mediaspdef,"\nMedia de la variable Speed: ", mediaspeed)
 media(df)
 
+def varianza(df):
+    print("="*50)
+    # varianza de la variable Total
+    varianzatotal = df["Total"].var()
+    # varianza de la variable Attack
+    varianzaataque = df["Attack"].var()
+    # varianza de la variable Defense
+    varianzadefensa = df["Defense"].var()
+    # varianza de la variable HP
+    varianzahp = df["HP"].var()
+    # varianza de la variable Sp. Atk
+    varianzaspatk = df["Sp. Atk"].var()
+    # varianza de la variable Sp. Def
+    varianzaspdef = df["Sp. Def"].var()
+    # varianza de la variable Speed
+    varianzaspeed = df["Speed"].var()
+    print("Varianza de la variable Total: ", varianzatotal,"\nVarianza de la variable Attack: ", varianzaataque,"\nVarianza de la variable Defense: ", varianzadefensa,"\nVarianza de la variable HP: ", varianzahp,"\nVarianza de la variable Sp. Atk: ", varianzaspatk,"\nVarianza de la variable Sp. Def: ", varianzaspdef,"\nVarianza de la variable Speed: ", varianzaspeed)
+varianza(df)
