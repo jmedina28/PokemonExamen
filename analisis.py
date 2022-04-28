fc2373ad6b08440a07ea76cd059266cbe22abc84
Juan Medina <91565820+jmedina28@users.noreply.github.com>
2022-04-28 09:31:41 +0200

    añado las medias

--- analisis.py
+++ analisis.py
@@ -4,5 +4,21 @@ import numpy as np
 df = pandas.read_csv("Pokemon.csv")
 print(df)
 
-# media de la variable Attack
-print(df["Attack"].mean())
+def media(df):
+    # media de la variable Total
+    mediatotal = df["Total"].mean()
+    # media de la variable Attack
+    mediaataque = df["Attack"].mean()
+    # media de la variable Defense
+    mediadefensa = df["Defense"].mean()
+    # media de la variable HP
+    mediahp = df["HP"].mean()
+    # media de la variable Sp. Atk
+    mediaspatk = df["Sp. Atk"].mean()
+    # media de la variable Sp. Def
+    mediaspdef = df["Sp. Def"].mean()
+    # media de la variable Speed
+    mediaspeed = df["Speed"].mean()
+    print("Media de la variable Total: ", mediatotal,"\nMedia de la variable Attack: ", mediaataque,"\nMedia de la variable Defense: ", mediadefensa,"\nMedia de la variable HP: ", mediahp,"\nMedia de la variable Sp. Atk: ", mediaspatk,"\nMedia de la variable Sp. Def: ", mediaspdef,"\nMedia de la variable Speed: ", mediaspeed)
+media(df)
+
